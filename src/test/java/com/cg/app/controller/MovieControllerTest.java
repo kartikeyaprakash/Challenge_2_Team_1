554b55a936184f68fef178244b3f736182994292
Prakash <kartikeya.prakash@capgemini.com>
2022-01-18 14:50:17 +0530

    Added tests and cleaned code

---
+++ src/test/java/com/cg/app/controller/MovieControllerTest.java
@@ -0,0 +1,91 @@
+package com.cg.app.controller;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.cg.app.service.MovieService;
+
+@ExtendWith(MockitoExtension.class)
+public class MovieControllerTest {
+	
+	@Mock
+	MovieService movieService;
+	
+	@InjectMocks
+	MovieController movieController;
+	
+	@Mock
+	List<String> testMovies;
+	
+	@Before
+	public void setup(){
+	    MockitoAnnotations.initMocks(this); 
+	}
+
+	
+	@Test
+	/**
+	 * Scenario: checks whether service method is called from controller classes
+	 * 
+	 * @throws IOException
+	 */
+	public void testGetMoviesByTheatre() throws IOException
+	{
+		String theatreName = "testTheatreName";
+		
+		when(movieService.getMoviesByTheatre(theatreName)).thenReturn(testMovies);
+		List<String> actual = movieController.getMoviesByTheatre(theatreName);
+		verify(movieService).getMoviesByTheatre(theatreName);
+		assertEquals(actual.size(), testMovies.size());
+		assertEquals(actual, testMovies);
+	}
+	
+	@Test
+	/**
+	 * Scenario: checks whether service method is called from controller classes
+	 * @throws IOException
+	 */
+	public void testGetMoviesByCity() throws IOException
+	{
+		String cityName = "testCityName";
+		
+		when(movieService.getMoviesByCity(cityName)).thenReturn(testMovies);
+		List<String> actual = movieController.getMoviesByCity(cityName);
+		verify(movieService).getMoviesByCity(cityName);
+		assertEquals(actual.size(), testMovies.size());
+		assertEquals(actual, testMovies);
+
+	}
+	
+	@Test
+	/**
+	 * Scenario: checks whether service method is called from controller classes
+	 * @throws IOException
+	 */
+	public void testGetMoviesAboveRating() throws IOException
+	{
+		Double rating = 5.0;
+		
+		when(movieService.getMoviesAboveRating(rating)).thenReturn(testMovies);
+		List<String> actual = movieController.getMoviesAboveRating(rating);
+		verify(movieService).getMoviesAboveRating(rating);
+		assertEquals(actual.size(), testMovies.size());
+		assertEquals(actual, testMovies);
+	}
+
+
+}
